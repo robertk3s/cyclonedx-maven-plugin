76ee66443c7229801a1ce34f32e731db48462675
Robert Klaus <robert.klaus@posteo.net>
2023-01-04 17:58:48 +0100

    Add another it test #111
    Signed-off-by: Robert Klaus <robert.klaus@posteo.net>

--- src/test/java/org/cyclonedx/maven/Issue111TestIT.java
+++ src/test/java/org/cyclonedx/maven/Issue111TestIT.java
@@ -2,8 +2,10 @@ package org.cyclonedx.maven;
 
 import static com.soebes.itf.extension.assertj.MavenITAssertions.assertThat;
 
+import com.soebes.itf.jupiter.extension.MavenCLIOptions;
 import com.soebes.itf.jupiter.extension.MavenGoal;
 import com.soebes.itf.jupiter.extension.MavenJupiterExtension;
+import com.soebes.itf.jupiter.extension.MavenOption;
 import com.soebes.itf.jupiter.extension.MavenTest;
 import com.soebes.itf.jupiter.maven.MavenExecutionResult;
 
@@ -13,7 +15,15 @@ public class Issue111TestIT
 
     @MavenTest
     @MavenGoal({"clean", "install"})
+    @MavenOption(MavenCLIOptions.NO_TRANSFER_PROGRESS)
     void test_makeAggregate_111(MavenExecutionResult result) {
+     assertThat(result).isSuccessful();
+    }
+
+    @MavenTest
+    @MavenGoal({"clean", "install"})
+    @MavenOption(MavenCLIOptions.NO_TRANSFER_PROGRESS)
+    void test_makeAggregate_fails(MavenExecutionResult result) {
      assertThat(result).isSuccessful();
     }
 
